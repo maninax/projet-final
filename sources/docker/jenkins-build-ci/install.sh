0ea91cf1a5d832ce7a196074c8d9b0d04555193d
Alexis Cassaigne <alexis.cassaigne@gmail.com>
2022-10-12 00:28:09 +0000

    Start the craft of a docker slave builder for Jenkins

---
+++ sources/docker/jenkins-build-ci/install.sh
@@ -0,0 +1,13 @@
+apt update
+apt install -y ca-certificates curl gnupg lsb-release
+mkdir -p /etc/apt/keyrings
+curl -fsSL https://download.docker.com/linux/debian/gpg | gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+echo \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/debian \
+  $(lsb_release -cs) stable" | tee /etc/apt/sources.list.d/docker.list > /dev/null
+apt update
+apt install -y docker-ce docker-ce-cli containerd.io docker-compose python3-pip
+apt install -y python3-pip
+pip install docker
+pip install ansible
+update-alternatives --install /usr/bin/python python /usr/bin/python3 100
