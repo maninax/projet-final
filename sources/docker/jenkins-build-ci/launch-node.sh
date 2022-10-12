6fe0a8c0404532c75cb43e50c5e158ada2ee0419
Alexis Cassaigne <alexis.cassaigne@gmail.com>
2022-10-12 17:47:46 +0000

    Jenkinsfile: debug sleep addedw

--- sources/docker/jenkins-build-ci/launch-node.sh
+++
@@ -1,6 +0,0 @@
-set -eu
-
-docker rmi -f ordiclic/jenkins-build-ci || true
-docker build . -t ordiclic/jenkins-build-ci
-docker push ordiclic/jenkins-build-ci:latest
-# docker run -d -p 23456:22 -v /var/run/docker.sock:/var/run/docker.sock ordiclic/jenkins-build-ci
